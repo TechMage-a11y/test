ff85979fd767370b9ee4fcdf867a31bea1992362
TechMage-a11y <King412006@outlook.com>
2024-09-23 13:42:36 +1200

    Измененено оформление

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 correct_logins = ["user_743", "user_123", "admin"]
 correct_passwords = ["cf63m", "pm5dw123", "py5en6"]
 
-print("__________________________")
+print("--------------------------")
 login = input("Введите логин: ")
-print("__________________________")
+print("--------------------------")
 password = input("Введите пароль: ")
-print("__________________________")
+print("--------------------------")
 
-print("__________________________")
+print("--------------------------")
 if login == correct_logins[0] and password == correct_passwords[0]:
     print("Добро пожаловать, user_743!")
 elif login == correct_logins[1] and password == correct_passwords[1]:
@@ -16,4 +16,4 @@ elif login == correct_logins[2] and password == correct_passwords[2]:
     print("Добро пожаловать, админ!")
 else:
     print("Неправильный логин или пароль. Попробуйте еще раз.")
-print("__________________________")
+print("--------------------------")
