d4eb5c6a21e12822b94313fbdc4218f3cc756c1a
TechMage-a11y <King412006@outlook.com>
2024-09-23 13:40:15 +1200

    Добавлена проверка логина и пароля

---
+++ main.py
@@ -0,0 +1,19 @@
+correct_logins = ["user_743", "user_123", "admin"]
+correct_passwords = ["cf63m", "pm5dw123", "py5en6"]
+
+print("__________________________")
+login = input("Введите логин: ")
+print("__________________________")
+password = input("Введите пароль: ")
+print("__________________________")
+
+print("__________________________")
+if login == correct_logins[0] and password == correct_passwords[0]:
+    print("Добро пожаловать, user_743!")
+elif login == correct_logins[1] and password == correct_passwords[1]:
+    print("Добро пожаловать, user_123!")
+elif login == correct_logins[2] and password == correct_passwords[2]:
+    print("Добро пожаловать, админ!")
+else:
+    print("Неправильный логин или пароль. Попробуйте еще раз.")
+print("__________________________")
